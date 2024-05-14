99fcd8c1abdb627d4f085c2b50a4843c40dec617
Nicholas Morey <nicholas@morey.tech>
2024-05-14 20:12:21 +0000

    chore: remove commented lines

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,11 +5,6 @@ echo "$(date): Running post-create.sh" >> ~/.status.log
 
 # this runs in background after UI is available
 
-# # https://github.com/k3d-io/k3d/issues/1169#issuecomment-1274554683
-# sudo prlimit --pid $$ --nofile=1048576:1048576
-# sudo sysctl fs.inotify.max_user_instances=1280
-# sudo sysctl fs.inotify.max_user_watches=655360
-
 k3d cluster create --config .devcontainer/k3d.yaml --wait --verbose 2>&1 | tee -a ~/.status.log
 
 echo "$(date): Finished post-create.sh" >> ~/.status.log
