3252d85aa8a2195803a527482996bcc6582ffff5
Nicholas Morey <nicholas@morey.tech>
2024-05-16 18:44:50 +0000

    feat: deploy apps

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -9,11 +9,12 @@ echo "$(date): Running post-start.sh" >> ~/.status.log
 k3d kubeconfig merge dev --kubeconfig-merge-default
 
 kubectl apply -k argocd 2>&1 | tee -a ~/.status.log
-# kubectl apply -f apps.yaml
 
 kubectl wait deployment -n argocd --all --for=condition=Available=True --timeout=90s 2>&1 | tee -a ~/.status.log
 ARGOCD_ADMIN_PASSWORD="$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
 echo "${ARGOCD_ADMIN_PASSWORD}" > ~/argo-cd-admin-password.txt
+
+kubectl apply -f apps.yaml 2>&1 | tee -a ~/.status.log
 
 argocd login \
   "localhost:31443" \
